8ed274e1936147bc09097504fb33a94656b026a2
pfellow <pfellow@gmail.com>
2023-04-18 19:22:06 +1200

    small refactoring

--- src/component/Meals/AvailableMeals.js
+++ src/component/Meals/AvailableMeals.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./AvailableMeals.module.css";
 import Meal from "./Meal";
 import Card from "../UI/Card";
@@ -6,30 +6,39 @@ import Card from "../UI/Card";
 export default function AvailableMeals() {
   const [isLoading, setIsloading] = useState(true);
   const [meals, setMeals] = useState([]);
-
-  const fetchMeals = useCallback(async () => {
-    const response = await fetch(
-      "https://react-http-b0b4e-default-rtdb.firebaseio.com/meals.json"
-    );
-    const result = await response.json();
-
-    setMeals(Object.entries(result)[0][1]);
-    setIsloading(false);
-  }, []);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
-    fetchMeals();
-  }, [fetchMeals]);
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-b0b4e-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-  const mealsList = isLoading ? (
-    <p>Loading...</p>
-  ) : meals.length > 0 ? (
-    meals.map((meal) => {
+      const result = await response.json();
+
+      setMeals(Object.entries(result)[0][1]);
+      setIsloading(false);
+    };
+
+    fetchMeals().catch((err) => {
+      setIsloading(false);
+      setHttpError(err.message);
+    });
+  }, []);
+
+  let mealsList = <p>Loading...</p>;
+
+  if (httpError) {
+    mealsList = <p>{httpError}</p>;
+  }
+  if (!isLoading && !httpError) {
+    mealsList = meals.map((meal) => {
       return <Meal key={meal.id} meal={meal} />;
-    })
-  ) : (
-    <p>No meals found!</p>
-  );
+    });
+  }
 
   return (
     <section className={styles.meals}>
